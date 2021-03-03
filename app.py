984dd299c00ae8c01430c37e64949ee74cd6277c
Taufik Amaryansyah <taufik.amary@gmail.com>
2021-03-03 22:33:45 +0700

    base layouting

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', contentheader='Dashboard')
 
 
 if __name__ == "__main__":
