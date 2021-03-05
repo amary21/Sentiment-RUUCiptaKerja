4d14320c46de0e381a4cdd8fa5c4c8f2b19140f8
Taufik Amaryansyah <taufik.amary@gmail.com>
2021-03-05 15:46:51 +0700

    form login

---
+++ controllers/login.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(
+        min=6, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
