64090b097ef5a5dbacb43cdff33a13444e952ba8
Taufik Amaryansyah <taufik.amary@gmail.com>
2021-05-23 18:37:52 +0700

    scheme

--- apps/extensions.py
+++ apps/extensions.py
@@ -30,9 +30,7 @@ class ReverseProxied(object):
         self.app = app
 
     def __call__(self, environ, start_response):
-        # if one of x_forwarded or preferred_url is https, prefer it.
-        forwarded_scheme = environ.get("HTTP_X_FORWARDED_PROTO", None)
-        preferred_scheme = self.app.config.get("PREFERRED_URL_SCHEME", None)
-        if "https" in [forwarded_scheme, preferred_scheme]:
+        scheme = environ.get("HTTP_X_FORWARDED_PROTO", None)
+        if scheme:
             environ["wsgi.url_scheme"] = "https"
         return self.app(environ, start_response)
