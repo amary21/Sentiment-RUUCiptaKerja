d5b818dc3ec72dff34426feb001bb8b8f2372740
Taufik Amaryansyah <taufik.amary@gmail.com>
2021-05-23 11:59:06 +0700

    https

--- apps/controllers/datasets/routes.py
+++ apps/controllers/datasets/routes.py
@@ -92,3 +92,12 @@ def download_csv():
     resp.headers["Content-Disposition"] = "attachment; filename=export_dataset.csv"
     resp.headers["Content-Type"] = "text/csv"
     return resp
+
+
+@datasets.before_request
+def before_request():
+    scheme = request.headers.get('X-Forwarded-Proto')
+    if scheme and scheme == 'http' and request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
